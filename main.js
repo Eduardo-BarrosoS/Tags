b35bfaf0097651d4781072fe22ef4a2064ed2e7f
Eduardo <barrosoeduardo64@gmail.com>
2022-05-28 14:36:29 -0300

    asdição do show w hide do input e a fuuncionalidaded de adicionar a tag atravez do click no iconSend

--- main.js
+++ main.js
@@ -1,11 +1,50 @@
 let tags = []
-let tagContainer = document.querySelector('.tag-container')
-let input =  tagContainer.querySelector('input')
+const tagContainer = document.querySelector('.tag-container')
+const tagContainerList = document.querySelector('.tagContainerList')
+const input =  tagContainer.querySelector('input')
+const iconPlus = tagContainer.querySelector('.plus')
+const iconSend= tagContainer.querySelector('.send')
+
+iconPlus.addEventListener('click', showInput)
+iconSend.addEventListener('click', hideInput)
+
+function hideInput() {
+    iconPlus.style.display='flex';
+    iconSend.style.display='none';
+    input.style.display='none';
+    input.style.animationName='hide';
+}
+
+function showInput(){
+    iconPlus.style.display='none';
+    iconSend.style.display='flex';
+    input.style.display='flex';
+    input.style.animationName='show';
+
+}
 
 input.addEventListener('keyup', addTags)
+iconSend.addEventListener('click', addTagsWithIconSend)
+
+
+function addTagsWithIconSend(event) {
+    const mousePressed = event.type == 'click'
+    
+    if(mousePressed) {
+
+        input.value.split(',').forEach( tag => {
+            if (tag) {
+                tags.push(tag.trim())
+            }
+        })
+    upDateTags()
+    input.value = ""
+    }
+  
+}
 
 function addTags(event) {
-    
+
     const keyPressedEnter = event.key == 'Enter'
     
     if(keyPressedEnter) {
@@ -18,54 +57,31 @@ function addTags(event) {
     upDateTags()
     input.value = ""
     }
-    
-
-    
-    
-    
+  
 }
 
 function upDateTags(){
     clearTags()
 
     tags.slice().reverse().forEach(tag => {
-        tagContainer.prepend(createTag(tag))
+        tagContainerList.prepend(createTag(tag))
     })
 }
 
 function createTag(tag) {
 
+
     const div = document.createElement('div')
     div.classList.add('tag') 
+    
 
     const span = document.createElement('span')
     span.innerHTML = tag
-
     div.append(span)
-
-    
-    // let item = ''
-    // let index = 0 
-    // tags.forEach(() => {
-    //     index = tags.length 
-
-    //     item += `
-    //          div class="tag">
-    //             <span>${input.value}</span>
-    //             <i onclick="removeTag(${index}) class="close"></i>
-    //          </div>
-    //          `
-    //          console.log(item)
-
-    //    tagContainer.innerHTML = item
-    // })
-    
-    
 
     const i = document.createElement('i')
     i.classList.add(`close`)
     i.setAttribute('data-item', tag)
-    // i.onclick = removeTag
     i.addEventListener('click', removeTag)
     span.append(i)
 
@@ -87,7 +103,9 @@ function removeTag(event) {
 }
 
 function clearTags(){
-    tagContainer.querySelectorAll('.tag')
+    tagContainerList.querySelectorAll('.tag')
         .forEach( tagElement => tagElement.remove())
 }
+
+
 
